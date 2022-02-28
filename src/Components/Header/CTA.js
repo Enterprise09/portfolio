c3ca56825d8c4398893663f25e4faf5719493c0d
Enterprise09 <goldenstaytes@naver.com>
2022-02-28 20:28:12 +0900

    [+] Header style

--- src/Components/Header/CTA.js
+++ src/Components/Header/CTA.js
@@ -7,7 +7,7 @@ function CTA() {
       <a href={CV} download className="btn">
         Download CV
       </a>
-      <a href="#contact" className="btn">
+      <a href="#contact" className="btn btn-primary">
         Let's Talk
       </a>
     </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 import CTA from "./CTA";
+import ME from "../../assets/images/me.jpg";
+import MEFULLSHOT from "../../assets/images/me_fullshot.png";
+import HeaderSocials from "./HeaderSocials";
+import "./Header.css";
 
 function Header() {
   return (
@@ -9,6 +13,13 @@ function Header() {
         <h1>TaeHo Kim</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
