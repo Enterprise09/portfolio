9759e2e20bc58fda020d7ea77f8e979807045d94
Enterprise09 <goldenstaytes@naver.com>
2022-02-28 18:30:01 +0900

    [-] clear all file for set up new version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React from "react";
-import About from "./Components/About";
-import Home from "./Components/Home";
-import Navigation from "./Components/Navigation";
-import "./css/App.css";
 
 function App() {
-  // will be add scroll event to .App
-
-  return (
-    <div className="App">
-      <Navigation />
-      <Home />
-      <About />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
